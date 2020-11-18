8a1dc3c5ec7bb4c4b931fb92b1787d0d49084d3b
Eric Zhao <21zhaoe@protonmail.com>
2020-11-17 21:56:25 -0500

    pkg/various(c): Finish migration to stew v0.2.0

--- packages/weechat/package.lua
+++ packages/weechat/package.lua
@@ -1,4 +1,7 @@
-local package = {}
-package.name = 'weechat'
-package.dependencies = {'../sh'}
-return package
+require('lib')
+
+pkg.name = 'weechat'
+pkg.dependencies:extend('../sh')
+
+-- Load local file if it exists
+require_opt('local')
